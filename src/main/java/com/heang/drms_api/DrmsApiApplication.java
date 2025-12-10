739cf3c336ffcd3e919e6fbbf46edec0259ba2fe
Hen-Heang <henheang15@gmail.com>
2025-12-10 14:46:07 +0900

    first commit

---
+++ src/main/java/com/heang/drms_api/DrmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.heang.drms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrmsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heang/drms_api/DrmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heang.drms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
